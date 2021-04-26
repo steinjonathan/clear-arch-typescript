eecc0fc94026bade9fe3267f0a143b16c2be17e1
Stein <jonathan.stein@saipos.com>
2021-04-26 20:25:02 -0300

    Add save purchases use case

---
+++ src/domain/usecases/save-purshases.ts
@@ -0,0 +1,13 @@
+interface SavePurchases {
+    save: (purchases: Array<SavePurchases.Params>) => Promise<void>
+}
+
+namespace SavePurchases {
+    export type Params = {
+        id: string,
+        date: Date,
+        value: number
+    }
+}
+
+export default SavePurchases
