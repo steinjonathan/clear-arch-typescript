9cdabcd9ece32ac528fc71edd770d013367467d7
Stein <jonathan.stein@saipos.com>
2021-04-26 21:06:43 -0300

    move production code to own files

---
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -0,0 +1,13 @@
+import { CacheStore } from "@/data/protocols/cache"
+
+export class LocalSavePurchases {
+    constructor(
+        private readonly cacheStore: CacheStore
+    ) {
+
+    }
+
+    async save(): Promise<void> {
+        this.cacheStore.delete('purchases')
+    }
+}
