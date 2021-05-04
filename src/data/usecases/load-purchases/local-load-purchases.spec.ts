bdc234707180259dc44f180b3b631ebe7e795d2c
Stein <jonathan.stein@saipos.com>
2021-05-03 21:01:02 -0300

    Should return an empty list if cache is more than 3 days old

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -32,8 +32,11 @@ describe('LocalLoadPurchases', () => {
     })
 
     test('Should return a list of purchases if cache is less than 3 days old', async () => {
-        const timestamp = new Date()
-        const { sut, cacheStore } = makeSut(timestamp)
+        const currentDate = new Date()
+        const timestamp = new Date(currentDate)
+        timestamp.setDate(timestamp.getDate() - 3)
+        timestamp.setSeconds(timestamp.getSeconds() + 1)
+        const { sut, cacheStore } = makeSut(currentDate)
         cacheStore.fetchResult = {
             timestamp,
             value: mockPurchases()
@@ -42,5 +45,22 @@ describe('LocalLoadPurchases', () => {
         expect(cacheStore.fetchKey).toEqual('purchases')
         expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch])
         expect(purchases).toEqual(cacheStore.fetchResult.value)
+    })
+
+    test('Should return an empty list if cache is more than 3 days old', async () => {
+        const currentDate = new Date()
+        const timestamp = new Date(currentDate)
+        timestamp.setDate(timestamp.getDate() - 3)
+        timestamp.setSeconds(timestamp.getSeconds() - 1)
+        const { sut, cacheStore } = makeSut(currentDate)
+        cacheStore.fetchResult = {
+            timestamp,
+            value: mockPurchases()
+        }
+        const purchases = await sut.loadAll()
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete])
+        expect(cacheStore.fetchKey).toEqual('purchases')
+        expect(cacheStore.deleteKey).toEqual('purchases')
+        expect(purchases).toEqual([])
     })
 })
--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -22,7 +22,13 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
     async loadAll () : Promise<Array<LoadPurchases.Result>> {
         try {
             const cache = this.cacheStore.fetch(this.PURCHASES_KEY)
-            return cache.value
+            const maxAge = new Date(cache.timestamp)
+            maxAge.setDate(maxAge.getDate() + 3)
+            if (maxAge > this.currentDate) {
+                return cache.value
+            } else {
+                throw new Error('Cache expirado')
+            }
         } catch (err) {
             this.cacheStore.delete(this.PURCHASES_KEY)
             return []
